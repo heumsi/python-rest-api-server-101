8704b477f9dba052a8378003d9d6704d54311c72
hardy <hardy@socar.kr>
2022-04-17 17:37:39 +0900

    feat(project): "시작하기"에 포함되는 파일들을 추가한다

---
+++ project/app.py
@@ -0,0 +1,98 @@
+import time
+from typing import Optional, List
+
+import uvicorn
+from fastapi import FastAPI, HTTPException, status, Query
+from fastapi.responses import PlainTextResponse
+from sqlmodel import Field, SQLModel, create_engine, Session, select
+
+
+class Post(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    title: str
+    author: str
+    content: str
+    created_at: Optional[int] = Field(default_factory=time.time)
+    updated_at: Optional[int] = Field(default_factory=time.time)
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+
+
+def create_db_and_tables() -> None:
+    SQLModel.metadata.create_all(engine)
+
+
+app = FastAPI()
+
+
+@app.get("/", response_class=PlainTextResponse, status_code=status.HTTP_200_OK)
+def healthcheck() -> str:
+    return "I'm Alive!"
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_post(new_post: Post) -> Post:
+    with Session(engine) as session:
+        session.add(new_post)
+        session.commit()
+        session.refresh(new_post)
+        return new_post
+
+
+@app.get("/posts/{post_id}", status_code=status.HTTP_200_OK)
+def read_post(post_id: int) -> Post:
+    with Session(engine) as session:
+        post = session.get(Post, post_id)
+        if not post:
+            raise HTTPException(status_code=404, detail="Post not found")
+        return post
+
+
+@app.get("/posts", status_code=status.HTTP_200_OK)
+def read_posts(offset: int = 0, limit: int = Query(default=100, lte=100)) -> List[Post]:
+    with Session(engine) as session:
+        statement = select(Post).offset(offset).limit(limit)
+        results = session.exec(statement)
+        posts = results.all()
+        return posts
+
+
+@app.put("/posts/{post_id}", status_code=status.HTTP_200_OK)
+def update_post(post_id: int, updated_post: Post) -> Post:
+    with Session(engine) as session:
+        post = session.get(Post, post_id)
+        if not post:
+            raise HTTPException(status_code=404, detail="Post not found")
+        post.updated_at = int(time.time())
+        updated_post_data = updated_post.dict(exclude_unset=True)
+        for key, value in updated_post_data.items():
+            setattr(post, key, value)
+        session.add(post)
+        session.commit()
+        session.refresh(post)
+        return post
+
+
+@app.delete("/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(post_id: int) -> None:
+    with Session(engine) as session:
+        statement = select(Post).where(Post.id == post_id)
+        results = session.exec(statement)
+        post = results.first()
+        if not post:
+            raise HTTPException(status_code=404, detail="Post not found")
+        session.delete(post)
+        session.commit()
+
+
+def main() -> None:
+    create_db_and_tables()
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+
+
+if __name__ == "__main__":
+    main()
