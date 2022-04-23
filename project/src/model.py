12a0ca52f41a98cf111bf818f150b5915bae1951
heumsi <heumsi@naver.com>
2022-04-23 17:03:35 +0900

    refactor: Model의 메타 데이터를 더 퐁성하게 표현한다

--- project/src/model.py
+++ project/src/model.py
@@ -8,10 +8,25 @@ def get_current_unix_timestamp() -> int:
     return int(time.time())
 
 
+title_field = Field(description="게시글 제목", min_length=1, max_length=100,)
+author_field = Field(description="게시글 작성자", min_length=1, max_length=30)
+content_field = Field(description="게시글 내용")
+schema_extra = {
+    "example": {
+        "title": "첫 번째 게시글 입니다!",
+        "author": "heumsi",
+        "content": "첫 번째 게시글 내용입니다!"
+    }
+}
+
+
 class PostBase(SQLModel):
-    title: str
-    author: str
-    content: str
+    title: str = Field(description="게시글 제목", min_length=1, max_length=100)
+    author: str = author_field
+    content: str = content_field
+
+    class Config:
+        schema_extra = schema_extra
 
 
 class Post(PostBase, table=True):
@@ -21,6 +36,6 @@ class Post(PostBase, table=True):
 
 
 class PostPatch(PostBase):
-    title: Optional[str]
-    author: Optional[str]
-    content: Optional[str]
+    title: Optional[str] = title_field
+    author: Optional[str] = author_field
+    content: Optional[str] = content_field
