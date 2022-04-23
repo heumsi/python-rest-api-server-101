aaeb508c8c4a47dc30d90de4ecd06b6d43eb0e76
hardy <hardy@socar.kr>
2022-04-23 14:58:18 +0900

    refactor: Model을 분리한다

--- project/src/model.py
+++ project/src/model.py
@@ -8,10 +8,13 @@ def get_current_unix_timestamp() -> int:
     return int(time.time())
 
 
-class Post(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+class PostBase(SQLModel):
     title: str
     author: str
     content: str
+
+
+class Post(PostBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     created_at: Optional[int] = Field(default_factory=get_current_unix_timestamp)
     updated_at: Optional[int] = Field(default_factory=get_current_unix_timestamp)
