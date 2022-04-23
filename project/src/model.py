61fddf9a6fe333fe707117c95a70e405483b5800
hardy <hardy@socar.kr>
2022-04-23 14:58:18 +0900

    refactor: Model을 분리한다

--- project/src/model.py
+++ project/src/model.py
@@ -4,10 +4,19 @@ from typing import Optional
 from sqlmodel import SQLModel, Field
 
 
-class Post(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+class PostBase(SQLModel):
     title: str
     author: str
     content: str
+
+
+class Post(PostBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     created_at: Optional[int] = Field(default_factory=time.time)
     updated_at: Optional[int] = Field(default_factory=time.time)
+
+
+class PostUpdate(SQLModel):
+    title: Optional[str] = None
+    author: Optional[str] = None
+    content: Optional[str] = None
