5d9f6e13904d6f919a6a56a9d0072ae3185a4ffb
heumsi <heumsi@naver.com>
2022-05-16 22:44:20 +0900

    refactor: /auth/signin 엔드포인트를 api/auth 패키지 내로 분리한다

--- project/src/api.py
+++ project/src/api.py
@@ -25,80 +25,8 @@ def healthcheck() -> str:
     return "I'm Alive!"
 
 
-tags = ["auth"]
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenPayload(BaseModel):
-    user: User
-
-
-JWT_SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-JWT_ALGORITHM = ALGORITHMS.HS256
-
-
-@app.post("/auth/signin", response_model=Token, tags=tags)
-def signin(form_data: OAuth2PasswordRequestForm = Depends()) -> Token:
-    with Session(engine) as session:
-        user = session.get(User, form_data.username)
-        if not user:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User does not exist")
-        if not pwd_context.verify(form_data.password, user.password):
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Password is incorrect")
-    token_payload = TokenPayload(
-        user=user
-    )
-    jwt_token = jwt.encode(
-        token_payload.dict(), key=JWT_SECRET_KEY, algorithm=JWT_ALGORITHM
-    )
-    token = Token(
-        access_token=jwt_token,
-        token_type="Bearer"
-    )
-    return token
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/signin")
-
 
 tags = ["user"]
-
-
-def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
-    try:
-        token_payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=JWT_ALGORITHM)
-        token_payload = TokenPayload(**token_payload)
-    except (JWTError, ValidationError):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token is not valid",
-        )
-    with Session(engine) as session:
-        user = session.get(User, token_payload.user.id)
-        if not user:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="User does not exist",
-            )
-    return user
-
-
-class GetAuthorizedUser:
-    def __init__(self, allowed_roles: List[Role]) -> None:
-        self._allowed_roles = allowed_roles
-
-    def __call__(self, user: User = Depends(get_current_user)) -> User:
-        if Role(user.role) not in self._allowed_roles:
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail="User does not authorized"
-            )
-        return user
 
 
 @app.get("/users",
