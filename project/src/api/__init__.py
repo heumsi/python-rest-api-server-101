c98fd8f0dc5c6d0f28c33b55553a8fcc732c84bb
heumsi <heumsi@naver.com>
2022-05-19 17:49:27 +0900

    refactor: / 엔드포인트를 api/__init__.py로 옮긴다

--- project/src/api/__init__.py
+++ project/src/api/__init__.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import PlainTextResponse
 
 from src.api import auth, users, posts
 from src.database import create_db_and_tables
@@ -17,3 +18,14 @@ app.include_router(posts.router)
 @app.on_event("startup")
 def handle_startup_event():
     create_db_and_tables()
+
+
+@app.get("/",
+    response_class=PlainTextResponse,
+    status_code=status.HTTP_200_OK,
+    summary="헬스체크용 엔드포인트 입니다.",
+    description="API 서버가 잘 작동하는지 확인합니다.",
+    response_description="API 서버가 잘 작동하고 있습니다.",
+)
+def healthcheck() -> str:
+    return "I'm Alive!"
