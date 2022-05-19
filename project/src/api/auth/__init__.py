dc090c8481f40201638a2257e468f150d5881315
heumsi <heumsi@naver.com>
2022-05-19 17:57:50 +0900

    refactor: 모든 엔드포인트의 API Summmary를 추가한다

--- project/src/api/auth/__init__.py
+++ project/src/api/auth/__init__.py
@@ -8,12 +8,14 @@ router.add_api_route(
     methods=["POST"],
     path="/signup",
     endpoint=signup.handle,
-    status_code=status.HTTP_201_CREATED
+    status_code=status.HTTP_201_CREATED,
+    summary="새로운 유저로 가입합니다.",
 )
 
 router.add_api_route(
     methods=["POST"],
     path="/signin",
     endpoint=signin.handle,
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
+    summary="로그인 합니다.",
 )
